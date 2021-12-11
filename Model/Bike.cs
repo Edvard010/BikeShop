823d247ea8e4ff7e8ed2ce3ee63d0761b44ec20f
Edvard010 <piszczatowski.grzegorz@gmail.com>
2021-12-11 12:25:38 +0100

    nie powinno się robić co chwilę commitów, ale to jest testowy commit, niech ludzie widzą klasy...

---
+++ Model/Bike.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BikeShop.Model
+{
+    public class Bike
+    {
+        public long Id { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Size { get; set; }
+        public string Description { get; set; }
+    }
+}
